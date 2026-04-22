16d7e2d65e5ea3e0095506cb1dd390c48452c553
Joshua Anderson <kimminjeongyoojimin1@gmail.com>
2026-04-22 18:38:47 +1000

    Updated main.c template

--- main.c
+++ main.c
@@ -1,5 +1,11 @@
-unsigned char* read_pgm(const char* filename, int* width, int* height);
-void write_pgm(const char* filename, unsigned char* data, int* width, int* height);
+unsigned char* read_pgm(const char* filename, int* width, int* height)
+{
+    return 0;
+}
+void write_pgm(const char* filename, unsigned char* data, int* width, int* height)
+{
+    
+}
 
 int main()
 {
